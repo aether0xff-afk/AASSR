971bcf634557bfc41a6b26fc7f7a115de771e21a
aether0xff-afk <aether0xff@gmail.com>
2026-07-28 20:14:14 +0900

    Export imagination tree components

--- src/aassr_v2/__init__.py
+++ src/aassr_v2/__init__.py
@@ -1,28 +1,57 @@
 """AASSR v2 research package."""
 
-from .evaluation import EvaluatedTransition, TransitionEvaluator
+from .confidence import AdaptiveDepthController
+from .evaluation import (
+    EvaluatedTransition,
+    ImaginationTransitionEvaluator,
+    PlannedTransition,
+    TransitionEvaluator,
+)
 from .gridworld import GridWorldEnv, GridWorldSpec
+from .imagination_tree import (
+    ImaginationConfig,
+    ImaginationNode,
+    ImaginationResult,
+    ImaginationTree,
+    RootActionEvaluation,
+    StateDeltaScorer,
+)
 from .information_value import InformationValueBreakdown, InformationValueWeights
 from .knowledge import KnowledgeDelta, KnowledgeEntry, KnowledgeStore
+from .policy import PolicyMemory, ScoredAction, WeightedPolicy
+from .prophecy import ProphecyStep
 from .tabular_prophecy import TabularProphecy
 from .types import Action, ActionVerb, Prediction, StateSnapshot, TransitionTrace
 
 __all__ = [
     "Action",
     "ActionVerb",
+    "AdaptiveDepthController",
     "EvaluatedTransition",
     "GridWorldEnv",
     "GridWorldSpec",
+    "ImaginationConfig",
+    "ImaginationNode",
+    "ImaginationResult",
+    "ImaginationTransitionEvaluator",
+    "ImaginationTree",
     "InformationValueBreakdown",
     "InformationValueWeights",
     "KnowledgeDelta",
     "KnowledgeEntry",
     "KnowledgeStore",
+    "PlannedTransition",
+    "PolicyMemory",
     "Prediction",
+    "ProphecyStep",
+    "RootActionEvaluation",
+    "ScoredAction",
+    "StateDeltaScorer",
     "StateSnapshot",
     "TabularProphecy",
     "TransitionEvaluator",
     "TransitionTrace",
+    "WeightedPolicy",
 ]
 
 __version__ = "0.1.0"
